4a3dc65faaf93f38a2bd13542740094009222c20
seanp02 <seanp02@naver.com>
2023-05-05 00:22:06 +0900

    hw8.py_local_variable_change

--- hw8.py
+++ hw8.py
@@ -4,17 +4,17 @@ def buy(lst):
     if item == '':
         return False
     num=int(input('수량은? '))
-    shopping_bag[item]=num
+    lst[item]=num
     print(f'장바구니에 {item} {num}개가 담겼습니다.\n')
 
 def show(lst):
-    print(f'\n>>> 장바구니 보기: {shopping_bag}\n')
+    print(f'\n>>> 장바구니 보기: {lst}\n')
 
 def find(lst):
     print('[검색]')
     search=input('장바구니에서 확인하고자 하는 상품은? ')
-    if search in shopping_bag:
-        res=shopping_bag[search]
+    if search in lst:
+        res=lst[search]
         print(f'{search}은(는) {res}개 담겨 있습니다.\n')
     else:
         print(f'장바구니에 {search}은(는) 없습니다.\n')
@@ -25,6 +25,7 @@ while True:
         break
 show(shopping_bag)
 find(shopping_bag)
+
 
 
 
